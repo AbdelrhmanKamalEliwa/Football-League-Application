ac1aefbe7719e21a6ef9bd1f44ff46672195647d
Abdelrhman Eliwa <abdelrhmankamaleliwa@gmail.com>
2020-11-12 18:44:48 +0200

    Initial Commit

---
+++ Football League Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football League Application
+//
+//  Created by Abdelrhman Eliwa on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
